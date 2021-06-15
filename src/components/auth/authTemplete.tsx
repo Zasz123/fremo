793bd2c3830be4d54496eb7860124eafb97c52f6
DESKTOP-LMDPJ93\Administrator <ygm12345@naver.com>
2021-06-15 22:40:31 +0900

    feat: apply common templete on auth templete

--- src/components/auth/authTemplete.tsx
+++ src/components/auth/authTemplete.tsx
@@ -1,30 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 
-const Templete = styled.div`
-  background-color: ${({ theme }) => theme.mainColor};
-  height: 100vh;
-`;
-
-const Wrapper = styled.div`
-  margin: 0 auto;
-  max-width: 1280px;
-  height: 100%;
-
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-const Block = styled.article`
-  width: 100%;
-  box-shadow: 0px 0px 50px grey;
-  height: 600px;
-
-  display: flex;
-  flex-direction: row;
-`;
-
+import { Templete, Wrapper, Block } from "../common/wrapper";
 interface IProps {
   children: React.ReactNode;
 }
@@ -33,7 +10,7 @@ const AuthTemplete = ({ children }: IProps) => {
   return (
     <Templete>
       <Wrapper>
-        <Block>{children}</Block>
+        <Block height={600}>{children}</Block>
       </Wrapper>
     </Templete>
   );

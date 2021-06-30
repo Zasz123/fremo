af3a49accbb640cc7201eb809691309e24a8f408
DESKTOP-LMDPJ93\Administrator <ygm12345@naver.com>
2021-06-30 22:35:52 +0900

    Refector: Change token type on redux

--- src/modules/auth/actions.ts
+++ src/modules/auth/actions.ts
@@ -1,9 +1,8 @@
 import { createAction } from "typesafe-actions";
 
 export const USER_LOGIN = "auth/USER_LOGIN";
-export const userLogin =
-  createAction(USER_LOGIN)<{
-    token: string;
-    email: string;
-    nickName: string;
-  }>();
+export const userLogin = createAction(USER_LOGIN)<{
+  token: string;
+  email: string;
+  nickName: string;
+}>();
--- src/modules/auth/interface.ts
+++ src/modules/auth/interface.ts
@@ -1,7 +1,7 @@
 import { AuthType } from "../../types/user/user";
 
 export interface AuthState {
-  token: string;
+  token: string | null;
   authMode: AuthType;
   isLoggedIn: boolean;
   info: {
